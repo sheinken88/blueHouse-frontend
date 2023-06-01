4c8a5249297de3fbeb4b0de890d482217f350385
PC <heinken.sebastian@gmail.com>
2023-06-01 12:13:25 -0300

    custom components buttons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
-import PrimaryButton from "../common/PrimaryButton"
+import {PrimaryButton, SecondaryButton, AddToCartButton} from "../common/Buttons"
+import { Grid } from "@mui/material";
 
 import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
@@ -20,10 +21,22 @@ export const HomePage = () => {
 
   return (
     <>
+      <Grid container direction="column" spacing={2}>
+      <Grid item>
       {products.slice(0,1).map((product) => (
          <div key={product.id}>{product.name}</div>
       ))}
+      </Grid>
+      <Grid item>
       <PrimaryButton>primary button</PrimaryButton>
+      </Grid>
+      <Grid item>
+      <SecondaryButton>secondary button</SecondaryButton>
+      </Grid>
+      <Grid item>
+      <AddToCartButton>add to cart</AddToCartButton>
+      </Grid>
+      </Grid>
     </>
   )
 }
