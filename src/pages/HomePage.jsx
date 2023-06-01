d46332ea4c0ba3cd8e8fff95378eb0ff749be387
Jose Moya <jose_moya@live.com.ar>
2023-06-01 18:39:35 -0300

    se realizadon cards de productos y primera version de grid

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,42 +1,45 @@
-import {PrimaryButton, SecondaryButton, AddToCartButton} from "../common/Buttons"
+import {
+  PrimaryButton,
+  SecondaryButton,
+  AddToCartButton,
+} from "../common/Buttons";
 import { Grid } from "@mui/material";
 
-import { useEffect } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { fetchAllProducts } from "../state/thunks/productsThunks"
-
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAllProducts } from "../state/thunks/productsThunks";
 
 export const HomePage = () => {
-  const dispatch = useDispatch()
-  const products = useSelector((state) => state.products.products)
+  // const dispatch = useDispatch()
+  const products = useSelector((state) => state.products.products);
   const isLoading = useSelector((state) => state.products.isLoading);
 
-  useEffect(() => {
-    dispatch(fetchAllProducts())
-  }, [dispatch])
-  
+  // useEffect(() => {
+  //   dispatch(fetchAllProducts())
+  // }, [dispatch])
+
   if (isLoading) {
-    return <div>Loading...</div>;  
+    return <div>Loading...</div>;
   }
 
   return (
     <>
       <Grid container direction="column" spacing={2}>
-      <Grid item>
-      {products.slice(0,1).map((product) => (
-         <div key={product.id}>{product.name}</div>
-      ))}
-      </Grid>
-      <Grid item>
-      <PrimaryButton>primary button</PrimaryButton>
-      </Grid>
-      <Grid item>
-      <SecondaryButton>secondary button</SecondaryButton>
-      </Grid>
-      <Grid item>
-      <AddToCartButton>add to cart</AddToCartButton>
-      </Grid>
+        <Grid item>
+          {products.slice(0, 1).map((product) => (
+            <div key={product.id}>{product.name}</div>
+          ))}
+        </Grid>
+        <Grid item>
+          <PrimaryButton>primary button</PrimaryButton>
+        </Grid>
+        <Grid item>
+          <SecondaryButton>secondary button</SecondaryButton>
+        </Grid>
+        <Grid item>
+          <AddToCartButton>add to cart</AddToCartButton>
+        </Grid>
       </Grid>
     </>
-  )
-}
+  );
+};
