fa4f8d778a9e6293726bacfbb4f33ed9daec6a68
Sebastian Heinken <sheinken@Sebastians-MacBook-Pro.local>
2023-06-02 18:13:00 -0300

    cart redux

--- src/state/slices/cartSlice.js
+++ src/state/slices/cartSlice.js
@@ -1,4 +1,53 @@
-// a confirmar.
+import { createSlice } from "@reduxjs/toolkit";
 
+const persistedState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : {
+      items: {},
+      totalPrice: 0,
+    };
 
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: persistedState,
+  reducers: {
+    addItemToCart: (state, action) => {
+      const newItem = action.payload;
+      const existingItem = state.items[newItem.id];
+      if (!existingItem) {
+        state.items[newItem.id] = {
+          ...newItem,
+        };
+        state.totalPrice += newItem.price;
+      } else {
+        existingItem.quantity++;
+        state.totalPrice += newItem.price;
+      }
+    },
+    removeItemFromCart: (state, action) => {
+      const id = action.payload;
+      const existingItem = state.items[id];
+      if (existingItem) {
+        state.totalPrice -= existingItem.price * existingItem.quantity;
+        delete state.items[id];
+      }
+    },
+    updateQuantity: (state, action) => {
+      const { id, quantity } = action.payload;
+      const existingItem = state.items[id];
+      if (existingItem) {
+        const quantityDifference = quantity - existingItem.quantity;
+        existingItem.quantity = quantity;
+        state.totalPrice += quantityDifference * existingItem.price;
+      }
+    },
+    clearCart: (state) => {
+      state.items = {};
+      state.totalPrice = 0;
+    },
+  },
+});
 
+export const { addItemToCart, removeItemFromCart, updateQuantity, clearCart } =
+  cartSlice.actions;
+export default cartSlice.reducer;
