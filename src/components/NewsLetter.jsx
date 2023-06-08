b3a4a99e1e37af0bcf9d0e52a52c67c8e18c64b4
bensznaider <bensznaider@yahoo.com.ar>
2023-06-08 16:53:17 -0300

    Newsletter: desktop design added

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -5,10 +5,15 @@ import {
   InputGroup,
   InputRightElement,
   Text,
+  HStack,
+  VStack,
+  Spacer,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import useInput from "../hooks/useInput.js";
 
 export const NewsLetter = () => {
+  const [mobileDesign] = useMediaQuery("(max-width: 425px)");
   const email = useInput();
 
   const handleSubmitEmail = (event) => {
@@ -18,42 +23,97 @@ export const NewsLetter = () => {
   };
 
   return (
-    <Box sx={{ bg: "primary", color: "fontHover", py: 8, textAlign: "center" }}>
-      <Box>
-        <Text sx={{ fontSize: 18 }}>SUBSCRIBE TO OUR NEWSLETTER</Text>
-        <Text sx={{ fontSize: 23, fontWeight: 900, color: "#A3D8E4" }}>
-          AND GET A 5% DISCOUNT
-        </Text>
-      </Box>
-      <InputGroup sx={{ my: 4, w: "100%" }}>
-        <Input
-          type="email"
-          placeholder="e-mail"
-          {...email}
+    <>
+      {mobileDesign ? (
+        <Box
+          sx={{ bg: "primary", color: "fontHover", py: 8, textAlign: "center" }}
+        >
+          <Box>
+            <Text sx={{ fontSize: 18 }}>SUBSCRIBE TO OUR NEWSLETTER</Text>
+            <Text sx={{ fontSize: 23, fontWeight: 900, color: "#A3D8E4" }}>
+              AND GET A 5% DISCOUNT
+            </Text>
+          </Box>
+          <InputGroup sx={{ my: 4, w: "100%" }}>
+            <Input
+              type="email"
+              placeholder="e-mail"
+              {...email}
+              sx={{
+                mx: 4,
+                color: "primary",
+                borderRadius: "2rem",
+                bg: "white",
+              }}
+            />
+            <InputRightElement>
+              <Button
+                sx={{
+                  borderRadius: "2rem",
+                  bg: "#A3D8E4",
+                  color: "primary",
+                  px: 10,
+                  mr: 16,
+                  fontWeight: "normal",
+                  fontSize: "small",
+                }}
+                onClick={handleSubmitEmail}
+              >
+                Subscribe
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </Box>
+      ) : (
+        <HStack
           sx={{
-            mx: 4,
-            color: "primary",
-            borderRadius: "2rem",
-            bg: "white",
+            bg: "primary",
+            color: "fontHover",
+            py: 10,
+            px: 40,
+            textAlign: "center",
           }}
-        />
-        <InputRightElement>
-          <Button
-            sx={{
-              borderRadius: "2rem",
-              bg: "#A3D8E4",
-              color: "primary",
-              px: 10,
-              mr: 16,
-              fontWeight: "normal",
-              fontSize: "small",
-            }}
-            onClick={handleSubmitEmail}
-          >
-            Subscribe
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-    </Box>
+        >
+          <VStack>
+            <Box>
+              <Text sx={{ fontSize: 27 }}>SUBSCRIBE TO OUR NEWSLETTER</Text>
+              <Text sx={{ fontSize: 35, fontWeight: 900, color: "#A3D8E4" }}>
+                AND GET A 5% DISCOUNT
+              </Text>
+            </Box>
+          </VStack>
+          <Spacer />
+          <InputGroup sx={{ my: 4, w: "45%" }}>
+            <Input
+              type="email"
+              placeholder="e-mail"
+              {...email}
+              sx={{
+                mx: 4,
+                color: "primary",
+                borderRadius: "2rem",
+                bg: "white",
+              }}
+            />
+            <InputRightElement>
+              <Button
+                sx={{
+                  borderRadius: "2rem",
+                  bg: "#A3D8E4",
+                  color: "primary",
+                  px: 10,
+                  mr: 16,
+                  fontWeight: "normal",
+                  fontSize: "small",
+                }}
+                onClick={handleSubmitEmail}
+              >
+                Subscribe
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </HStack>
+      )}
+    </>
   );
 };
