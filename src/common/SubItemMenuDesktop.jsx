7c50f38293b927d532118f159f697ab4077a2b88
bensznaider <bensznaider@yahoo.com.ar>
2023-06-21 18:19:36 -0300

    Navbar: link to productDesk added (desktop v.)

--- src/common/SubItemMenuDesktop.jsx
+++ src/common/SubItemMenuDesktop.jsx
@@ -3,10 +3,9 @@ import {
   HStack,
 } from "@chakra-ui/react";
 import { ImageMenuItem } from "./ImageMenuItem";
+import he from "he"
 
 export const SubItemMenuDesktop = ({ category }) => {
-  let categoryName = category.name.split("amp;");
-  categoryName = categoryName.join("");
 
   return (
     <Box
@@ -19,7 +18,7 @@ export const SubItemMenuDesktop = ({ category }) => {
       <HStack>
         <ImageMenuItem pl={"1rem"} category={category} />
         <Box pl={"1rem"} flex="1" textAlign="left">
-          {categoryName}
+          {he.decode(category.name)}
         </Box>
       </HStack>
     </Box>
